721690c1c4b0699a8f6f10c1e891c56ee7912648
addisonfreeman <afree1423@gmail.com>
2016-08-03 11:49:17 -0500

    remove custom_config configuration that exists in base module, add module folders that are dependencies

---
+++ modules/fivestar-8.x-2.x/src/Plugin/Field/FieldWidget/FiveStartWidgetBase.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\fivestar\Plugin\Field\FieldWidget\FiveStartWidgetBase
+ */
+
+namespace Drupal\fivestar\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\WidgetBase;
+
+/**
+ * Class FiveStartWidgetBase
+ * @package Drupal\fivestar\Plugin\Field\FieldWidget
+ */
+abstract class FiveStartWidgetBase extends WidgetBase {
+
+  /**
+   * @return array
+   */
+  protected function getAllWidget() {
+    return \Drupal::moduleHandler()->invokeAll('fivestar_widgets');
+  }
+
+}
