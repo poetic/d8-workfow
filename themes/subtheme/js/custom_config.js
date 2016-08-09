856cae59b0237f44a0a47d0e57888577fa93ad36
addisonfreeman <afree1423@gmail.com>
2016-08-09 11:48:40 -0500

    form complete

--- themes/subtheme/js/custom_config.js
+++ themes/subtheme/js/custom_config.js
@@ -1,1 +1,14 @@
 $ = jQuery;
+
+console.log("custom js working");
+$('.action-unflag').on('click', function() {
+  ct = parseInt($('.rcp-main.saved-tally').html());
+  console.log(ct);
+  $('.rcp-main.saved-tally').html(ct-1);
+});
+
+$('.action-flag').on('click', function() {
+  ct = parseInt($('.rcp-main.saved-tally').html());
+  console.log(ct);
+  $('.rcp-main.saved-tally').html(ct+1);
+});
